904540ee37c9bd446fab6adcfeab5a83e4bb369a
SigureMo <sigure.qaq@gmail.com>
2022-01-01 17:49:39 +0800

    🚧 wip: add more example

--- pycc/__main__.py
+++ pycc/__main__.py
@@ -1,7 +1,7 @@
 import argparse
 
 # from pycc.vm import VirtualMachine, Instruction, c_pointer_to_string, c_pointer_to_integer
-from pycc.lexer import Token
+from pycc.lexer import Token, match_comments, match_whitespace
 
 
 def main():
@@ -13,19 +13,44 @@ def main():
     if extra_args:
         print("[INFO] Extra arguments: ", " ".join(extra_args))
 
+    # 读入源代码
     with open(args.src, "r") as f:
         source_code = f.read()
 
+    # 词法分析
     i = 0
     token_stream: list[Token] = []
     while i < len(source_code):
+        print(source_code[i])
+        # 匹配并跳过注释
+        if match_obj := match_comments(source_code, i):
+            comments = match_obj.group(0)
+            i += len(comments)
+            continue
+
+        # 匹配并跳过空白字符
+        if match_obj := match_whitespace(source_code, i):
+            whitespace = match_obj.group(0)
+            i += len(whitespace)
+            continue
+
+        # 匹配各种 token
+        continue_flag = False
         for token_cls in Token.token_classes():
             if match_obj := token_cls.match(source_code, i):
                 match_str = match_obj.group(0)
                 i += len(match_str)
                 token_stream.append(token_cls(match_str))
+                # Python 无法直接跳出两层，因此通过 flag 来处理
+                continue_flag = True
+                continue
+        if continue_flag:
+            continue
+
+        # 未预期的符号
         else:
-            i += 1
+            # i += 1
+            raise Exception(f"Unexpected symbol: {source_code[i]}")
     print(token_stream)
 
 
--- pycc/lexer.py
+++ pycc/lexer.py
@@ -1,15 +1,28 @@
 import re
 from dataclasses import dataclass
-from typing import Any
+from typing import Any, Optional
+from enum import Enum
+
+
+class CType(Enum):
+
+    int = 0
+    float = 1
+    char = 2
+    ptr = 3
+
+    # TODO: ptr to type
 
 
 @dataclass
 class Token:
     value: Any = None
     regexp = re.compile(r"")
+    has_value = False
 
     def __init__(self, string: str):
-        self.value = string
+        if self.has_value:
+            self.value = string
 
     @classmethod
     def match(cls, string: str, pos: int):
@@ -23,7 +36,78 @@ class Token:
 class Num(Token):
     value: int
     regexp = re.compile(r"\d+")
+    has_value = True
 
     def __init__(self, string: str):
         # TODO: 二进制、八进制、浮点数支持
         self.value = eval(string)
+
+
+# class Fun(Token):
+# class Sys(Token):
+# class Glo(Token):
+# class Loc(Token):
+# class Id(Token):
+# class Char(Token):
+# class Else(Token):
+# class Enum(Token):
+# class If(Token):
+class Int(Token):
+    value: None
+    regexp = re.compile(r"int")
+
+
+class Return(Token):
+    value: None
+    regexp = re.compile(r"return")
+
+
+class Sizeof(Token):
+    value: None
+    regexp = re.compile(r"sizeof")
+
+
+# class While(Token):
+# class Assign(Token):
+# class Cond(Token):
+# class Lor(Token):
+# class Lan(Token):
+# class Or(Token):
+# class Xor(Token):
+# class And(Token):
+# class Eq(Token):
+# class Ne(Token):
+# class Lt(Token):
+# class Gt(Token):
+# class Le(Token):
+# class Ge(Token):
+# class Shl(Token):
+# class Shr(Token):
+# class Add(Token):
+# class Sub(Token):
+# class Mul(Token):
+# class Div(Token):
+# class Mod(Token):
+# class Inc(Token):
+# class Dec(Token):
+# class Brak(Token):
+
+
+def match_comments(string: str, pos: int) -> Optional[re.Match[str]]:
+    regexp_comments_double_slash = re.compile(r"//[\s\S]*?(\n|$)")
+    regexp_comments_slash_star = re.compile(r"/\*[\s\S]*?\*/")
+    if string.startswith("//", pos):
+        match_obj = regexp_comments_double_slash.match(string, pos)
+        assert match_obj is not None, "[Error] Unterminated comment"
+        return match_obj
+    elif string.startswith("/*", pos):
+        match_obj = regexp_comments_slash_star.match(string, pos)
+        assert match_obj is not None, "[Error] Unterminated comment"
+        return match_obj
+    else:
+        return None
+
+
+def match_whitespace(string: str, pos: int) -> Optional[re.Match[str]]:
+    regexp_whitespace = re.compile(r"[\s]+")
+    return regexp_whitespace.match(string, pos)
